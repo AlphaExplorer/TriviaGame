748778a4a13cb37277d293e52b9773a8451a10eb
Francisco Gonzalez <francisco@primestaram.com>
2019-07-15 18:16:13 -0400

    initial commit

---
+++ assets/javascript/app.js
@@ -0,0 +1,13 @@
+var question_answers = 
+[
+  {question: "", image: "", answer: ""},
+  {question: "", image: "", answer: ""},
+  {question: "", image: "", answer: ""},
+  {question: "", image: "", answer: ""},
+  {question: "", image: "", answer: ""},
+  {question: "", image: "", answer: ""},
+  {question: "", image: "", answer: ""},
+  {question: "", image: "", answer: ""},
+  {question: "", image: "", answer: ""},
+  {question: "", image: "", answer: ""},
+];
